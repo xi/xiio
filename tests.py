6b9965a49151195a363b7eb06f53a6379e1102ea
Tobias Bengfort <tobias.bengfort@posteo.de>
2026-02-27 01:04:20 +0100

    simple sync test functions

--- tests.py
+++ tests.py
@@ -16,4 +16,36 @@ class XiioTestCase(unittest.TestCase):
 
 
 class TestRun(XiioTestCase):
-    pass
+    def test_sleep(self):
+        def foo():
+            time.sleep(0.1)
+            return 'Hello World'
+
+        with self.assert_duration(0.1):
+            result = foo()
+        self.assertEqual(result, 'Hello World')
+
+    def test_runs_cleanup_on_error_while_paused(self):
+        stack = []
+
+        def foo():
+            try:
+                time.sleep(0.1)
+            finally:
+                stack.append(1)
+
+        with mock.patch('time.sleep', side_effect=KeyboardInterrupt):
+            with self.assertRaises(KeyboardInterrupt):
+                foo()
+        self.assertEqual(stack, [1])
+
+    def test_waits_for_cleanup(self):
+        def foo():
+            try:
+                raise ValueError
+            finally:
+                time.sleep(0.1)
+
+        with self.assertRaises(ValueError):
+            with self.assert_duration(0.1):
+                foo()
